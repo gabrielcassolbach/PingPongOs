fe47a6ad8acd42cc1a740c27a14ee61bbf2e7175
gabrielbach <gabrielcassolbach@gmail.com>
2024-09-16 14:50:29 -0300

    queue_remove

--- queue.c
+++ queue.c
@@ -1,9 +1,6 @@
 #include "queue.h"
 #include <stdio.h>
 
-//------------------------------------------------------------------------------
-// Conta o numero de elementos na fila
-// Retorno: numero de elementos na fila
 int queue_size (queue_t * queue) {
 	if(queue == NULL) return 0;
 	int size = 1; queue_t *it = queue;
@@ -14,18 +11,20 @@ int queue_size (queue_t * queue) {
 	return size;
 }
 
-// Percorre a fila e imprime na tela seu conteúdo. A impressão de cada
-// elemento é feita por uma função externa, definida pelo programa que
-// usa a biblioteca. Essa função deve ter o seguinte protótipo:
-//
-// void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
 void queue_print (char *name, queue_t *queue, void print_elem (void*)){
 		
+}
+
+int cantAppendElem(queue_t *elem) {
+	if(elem->next != NULL || elem->prev != NULL) return 1;
+	return 0;
 }
 
 int queue_append (queue_t **queue, queue_t *elem) {
 	if(queue == NULL) {puts("queue don't exist"); return -1;}
 	if(elem  == NULL) {puts("element don't exist"); return -1;}
+
+	if(cantAppendElem(elem)) {puts("element is in another queue"); return -1;}
 
 	if(queue_size(*queue) == 0) {
 		elem->next = elem; 
@@ -41,30 +40,50 @@ int queue_append (queue_t **queue, queue_t *elem) {
 	return 0;
 }
 
-//------------------------------------------------------------------------------
-// Remove o elemento indicado da fila, sem o destruir.
-// Condicoes a verificar, gerando msgs de erro:
-// - a fila deve existir -- ok.
-// - a fila nao deve estar vazia -- ok.
-// - o elemento deve existir -- ok.
-// - o elemento deve pertencer à fila indicada -- ok. 
-// Retorno: 0 se sucesso, <0 se ocorreu algum erro
-int queue_remove (queue_t **queue, queue_t *elem) {
-	if(queue == NULL) {puts("queue don't exist"); return -1;}
-	if(elem  == NULL) {puts("element don't exist"); return -1;}
+int inRightQueue(queue_t *it, queue_t *elem) {
+	queue_t *first_elem = it;
+	do{
+		if(it == elem) return 1;
+		it = it->next;
+	} while(it != first_elem);
+	return 0;
+}
 
-	queue_t *it = (*queue);
-	int counter = 0; int q_size = queue_size((*queue));
-	while(it != (elem) && counter != q_size)
-		{counter++; it = it->next;}
+int cantRemove(queue_t **queue, queue_t *elem) {
+	if(queue == NULL) {puts("queue don't exist"); return 1;}
+	if(*queue == NULL) {puts("empty queue"); return 1;}
+	if(elem  == NULL) {puts("element don't exist"); return 1;}
+	return 0;
+}
+
+void eliminateQueue(queue_t **queue) {
+	(*queue)->next = (*queue)->prev = NULL;
+	(*queue) = NULL;
+}
+
+void cutQueue(queue_t **queue, queue_t *elem){
+	if((*queue) == elem) (*queue) = elem->next;
+
+	queue_t *nextIt = elem->next;
+	queue_t *prevIt = elem->prev;
+
+	nextIt->prev = prevIt;
+	prevIt->next = nextIt;
+	elem->next = elem->prev = NULL;
+}
+
+int queue_remove (queue_t **queue, queue_t *elem) {
+	if(cantRemove(queue, elem)) {return -1;}
+
+	if (!inRightQueue((*queue), elem)) {
+		puts("The element doesn't belong to the right queue");	
+		return 0;
+	}
+
+	if(queue_size(*queue) > 1) 
+		cutQueue(queue, elem);
+	else 
+		eliminateQueue(queue);
 	
-	if(it != elem)
-		{puts("element don't belong to queue..."); return -1;}
-	
-	it->prev->next = it->next; 
-	it->next->prev = it->prev;
-	if(counter == 0) 
-		(*queue) = it->next;
-	it->next = NULL; it->prev = NULL;
 	return 0;
 }
